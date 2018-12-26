5f6c51e24001b162990ed847f55e9a78a016b697
Naoya Yamashita <naoya3e@gmail.com>
2018-12-26 18:46:18 +0900

    style: fix by linter

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import styled from '@emotion/styled'
 import React from 'react'
 
 const App: React.FunctionComponent = () => (
-  <Logo xmlns="http://www.w3.org/2000/svg"
-       width="200px" height="200px"
-       viewBox="0 0 200 200">
+  <Logo xmlns="http://www.w3.org/2000/svg" width="200px" height="200px" viewBox="0 0 200 200">
     <circle cx="100" cy="100" r="40" fill="none" stroke="black" stroke-width="3" />
   </Logo>
 )
