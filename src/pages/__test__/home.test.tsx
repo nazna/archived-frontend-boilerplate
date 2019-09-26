5cb52fb3dca823309a667ad452d2e6009bebcdc4
Naoya Yamashita <naoya3e@gmail.com>
2019-09-26 15:35:27 +0900

    test: fix test case for routing
    routing component test required MemoryRouter provided by react-router-dom

--- src/pages/__test__/home.test.tsx
+++ src/pages/__test__/home.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 import Home from '../home'
 
 describe('pages', () => {
   describe('home', () => {
     test('<Home />', () => {
-      const rendered = render(<Home />)
+      const rendered = render(
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      )
       expect(rendered).toBeTruthy()
     })
   })
