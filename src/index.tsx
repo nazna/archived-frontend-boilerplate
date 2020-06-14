4b40bd3f38da45110169e4c0d22e09d2f3ad878f
nazna <14846330+nazna@users.noreply.github.com>
2020-06-14 20:31:19 +0900

    refactor: remove error-boundary

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import loadable from '@loadable/component'
 import { AppBar } from './components/modules/app-bar'
 import { GlobalStyles } from './components/abstracts/global-styles'
-import { ErrorBoundary } from './components/abstracts/error-boundary'
 
 const Home = loadable(() => import(/* webpackPreload: true */ './pages/home'))
 const Reddit = loadable(() => import(/* webpackPreload: true */ './pages/reddit'))
@@ -15,17 +14,15 @@ function App() {
   return (
     <Fragment>
       <GlobalStyles />
-      <ErrorBoundary>
-        <BrowserRouter>
-          <AppBar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/reddit" component={Reddit} />
-            <Route exact path="/todo" component={Todo} />
-            <Route component={NotFound} />
-          </Switch>
-        </BrowserRouter>
-      </ErrorBoundary>
+      <BrowserRouter>
+        <AppBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/reddit" component={Reddit} />
+          <Route exact path="/todo" component={Todo} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </Fragment>
   )
 }
